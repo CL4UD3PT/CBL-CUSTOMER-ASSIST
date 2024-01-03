e27a3ef0a29652d16d0e8ee3f2b253a2fb36a1fd
CL4UD3PT <98550012+CL4UD3PT@users.noreply.github.com>
2024-01-03 23:50:37 +0000

    replaced react-multiselect by react-select

--- src/front/js/pages/customer/customer_equipment_list.js
+++ src/front/js/pages/customer/customer_equipment_list.js
@@ -1,11 +1,8 @@
 import React, { useState, useContext, useEffect } from "react";
-import { Multiselect } from 'multiselect-react-dropdown';
 import { Context } from "../../store/appContext";
 import { EquipmentListCard } from "../../component/equipment_list_card";
 import { useLocation } from "react-router-dom";
-// import Select from 'react-select';
-// import EquipmentIMG from "../../../assets/img/help_description/Equipment-IMG.png";
-// import EquipmentDescription from "../../../assets/img/help_description/Equipment-History-Google-Docs.png";
+import Select from 'react-select';
 
 export const CustomerEquipmentList = () => {
     const { actions, store } = useContext(Context);
@@ -17,30 +14,31 @@ export const CustomerEquipmentList = () => {
         actions.getCustomerEquipment();
     }, []);
 
-    const handleEquipment = (selectedOptions) => {
-        setFilteredEquipment(selectedOptions.map(option => option));
-        console.log(filteredEquipment);
-    };
+    const handleEquipment = (options, action) => {
+        setFilteredEquipment(options.map(option => option));
+    }
 
     return (
-        <main className="bd-main order-1 pe-4">
-            <div className="bd-intro border-bottom d-flex justify-content-end">
+        <>
+            <main className="bd-main order-1 pe-4">
+                <div className="bd-intro border-bottom d-flex justify-content-end">
 
-                {/* <!-- Button trigger modal --> */}
-                <div>
-                    <strong typeof="button" className="bd-links-heading btn d-flex w-100 align-items-center fw-semibold" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                        <i className="fa-solid fa-circle-question me-1" style={{ color: "blue" }}></i>Help
-                    </strong>
+                    {/* <!-- Button trigger modal --> */}
+                    <div>
+                        <strong typeof="button" className="bd-links-heading btn d-flex w-100 align-items-center fw-semibold" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                            <i className="fa-solid fa-circle-question me-1" style={{ color: "blue" }}></i>Help
+                        </strong>
+                    </div>
                 </div>
-            </div>
-            <div className="bd-content">
-                <div>
-                    <div className="mb-3 p-3 col-sm-12 col-md-8 col-lg-8 mx-auto d-flex ">
-                        <h5 className="me-3 mt-2">Equipment:</h5>
+                <div className="bd-content">
+                    <div>
+                        <div className="d-flex flex-column">
+                            <h5 className="me-3 mt-2">Equipment:</h5>
 
-                        {/* APLICAR O REACT-SELECT NA PROCURA */}
-                        {/* <Select
+                            {/* FILTER EQUIPMENTS */}
+                            <Select
                                 id="selectEquipment"
+                                isMulti
                                 className="basic-single mb-2"
                                 classNamePrefix="select"
                                 isSearchable={false}
@@ -48,61 +46,54 @@ export const CustomerEquipmentList = () => {
                                 isDisabled={false}
                                 // components={animatedComponents}
                                 options={equipmentList}
-                                // defaultValue={assignedVehicle}
+                                getOptionLabel={(option) => `${option.model}`}
+                                getOptionValue={(option) => `${option.model}`}
                                 styles={{
                                     control: (baseStyles, state) => ({
                                         ...baseStyles,
                                     }),
                                 }}
                                 onChange={(newValue, actionMeta) => {
-                                    if (actionMeta.action === 'select-option') setSelectedCategoryID(newValue.id);
-                                    // if (actionMeta.action === 'clear') handleDismissVehicleFromTicket();
+                                    handleEquipment(newValue, actionMeta);
+                                    // if (actionMeta.action === 'clear') handleDismissEquipment(newValue, actionMeta);
                                 }}
-                            /> */}
-                            
-                        <Multiselect
-                            // options={filterByValues(equipmentList, 'model', equipmentSelected)}
-                            options = {equipmentList}
-                            displayValue="model"
-                            placeholder="Select equipment"
-                            onSelect={handleEquipment}
-                            onRemove={handleEquipment}
-                        />
-                    </div>
-                </div>
-                <div className="bd-content ">
-                    <div className="border rounded p-4 flex-fill">
-                        <div>
-                            {filteredEquipment.length > 0
-                                ? filteredEquipment.map((item, i) => { return <EquipmentListCard key={i} data={item} /> })
-                                : equipmentList.map((item, i) => { return <EquipmentListCard key={i} data={item} /> })
-                            })
+                            />
                         </div>
                     </div>
-                </div>
-                {/* <!-- Modal --> */}
-                <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                    <div className="modal-dialog modal-fullscreen">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h1 className="modal-title fs-5" id="staticBackdropLabel">Equipment List Explained</h1>
-                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div className="bd-content ">
+                        <div className="border rounded p-4 flex-fill">
+                            <div>
+                                {filteredEquipment.length > 0
+                                    ? filteredEquipment.map((item, i) => { return <EquipmentListCard key={i} data={item} /> })
+                                    : equipmentList.map((item, i) => { return <EquipmentListCard key={i} data={item} /> })
+                                }
                             </div>
-                            <div className="modal-body d-flex">
-                                <div >
-                                    <img className="p-2 me-4 border rounded-3" src="https://res.cloudinary.com/dsonpr8ip/image/upload/v1689793345/Equipment-History-Google-Docs_t62efj.png" />
+                        </div>
+                    </div>
+                    {/* <!-- Modal --> */}
+                    <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                        <div className="modal-dialog modal-fullscreen">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h1 className="modal-title fs-5" id="staticBackdropLabel">Equipment List Explained</h1>
+                                    <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                 </div>
-                                <div >
-                                    <img className="border rounded-3" style={{ height: "620px", width: "1200px" }} src="https://res.cloudinary.com/dsonpr8ip/image/upload/v1689793345/Equipment-IMG_fy2owf.png" />
+                                <div className="modal-body d-flex">
+                                    <div >
+                                        <img className="p-2 me-4 border rounded-3" src="https://res.cloudinary.com/dsonpr8ip/image/upload/v1689793345/Equipment-History-Google-Docs_t62efj.png" />
+                                    </div>
+                                    <div >
+                                        <img className="border rounded-3" style={{ height: "620px", width: "1200px" }} src="https://res.cloudinary.com/dsonpr8ip/image/upload/v1689793345/Equipment-IMG_fy2owf.png" />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <div className="modal-footer">
+                                    <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </main>
+            </main>
+        </>
     );
 };
